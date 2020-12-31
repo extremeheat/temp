52310d8ff78f501740fc16750cb04dd96715a421
extremeheat <extreme@protonmail.ch>
2020-12-31 07:17:41 -0500

    artifact test

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const fs = require('fs')
+
 var shouldFail = false
 
 module.exports = {
     hello: function() {
         if (process.env.FAIL || shouldFail) return 'nope!!'
         return 'hello world!'
+    },
+
+
+    writeFile: function() {
+        let fname = 'File'+ Date.now() + '.dat'
+        fs.writeFileSync(fname, 'MyFileData for ' + process.platform)
+        return fname
     }
 }
--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 const test = require('./index')
 
 if (test.hello() === 'hello world!') {
+    
+    let fname = test.writeFile()
+    if (fname) {
+        console.log('::set-output name=BUILT::' + fname)
+    }
+    
     process.exit(0)
 } else {
     process.exit(1)
